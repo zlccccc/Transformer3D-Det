c2846d2aa9514fb9bca69ee62005efcb64f7a9b7
zhaolichen <zlc1114@buaa.edu.cn>
2020-10-03 11:52:46 +0800

    fix commit model init way

--- core/model/Bow_model/__init__.py
+++ core/model/Bow_model/__init__.py
@@ -1,7 +1,4 @@
-import torch.nn as nn
-import importlib
-import os
-import re
+from ..subfile_init import sub_model_entry
 
 module_dict = {
     'BowModel': 'BowModel.BowModel',
@@ -9,29 +6,4 @@ module_dict = {
 
 
 def model_entry(config):
-    package_name = __file__
-    print('package name', package_name)
-    package_name = re.split('\\/', package_name)[-2]
-    name = config.name
-    print('try loading from model package %s: ' % package_name, module_dict.keys())
-    if name not in module_dict.keys():
-        return None
-    del config['name']
-    if config.get('use_syncbn', False):
-        print('using SyncBatchNorm')
-        raise NotImplementedError('SyncBatchNorm')
-    print('get config from %s MyImpilement' % package_name)
-    print('module config:', config.keys())
-    try:
-        pathlist = 'core.model.' + package_name + '.' + module_dict[name]  # must abspath
-        pathlist = pathlist.split('.')
-        relativepath, packagepath = pathlist[-1], '.'.join(pathlist[:-1])
-        print('try to import_module', relativepath, packagepath)
-        package = importlib.import_module(packagepath)
-        assert hasattr(package, relativepath), 'should have class in python file'
-        modelclass = getattr(package, relativepath)
-    except Exception as e:
-        print('model path', pathlist, 'not exist')
-        print(str(e))
-        modelclass = None
-    return modelclass(config)
+    return sub_model_entry(config, __file__, module_dict)
--- core/model/__init__.py
+++ core/model/__init__.py
@@ -1,24 +1,20 @@
-import torch.nn as nn
-
-from .Bow_model import model_entry as BowModel
-from .Pointnet import model_entry as Pointnet
-from .PointnetYanx27 import model_entry as PointnetInitial
-from .SGmodel import model_entry as SGmodel
-from .RandLANet import model_entry as RandLANet
-
-
-model_entry_name = [
-    Pointnet,
-    PointnetInitial,
-    BowModel,
-    SGmodel,
-    RandLANet,
-]
+import importlib
+import os
+import re
 
 
 def model_entry(config):
-    for func in model_entry_name:
-        model = func(config)
-        if model is not None:
-            return model
+    print(__file__, __path__, '<<< file and path')
+    for dirname in os.listdir(__path__[0]):
+        packagepath = 'core.model.' + dirname
+        print('try to load from ', dirname, packagepath)
+        try:
+            packagemodule = importlib.import_module(packagepath)
+            func = packagemodule.model_entry
+            model = func(config)
+            if model is not None:
+                return model
+        except Exception as e:
+            print('try loading: ', str(e))
+            continue  # no model_entry
     raise NotImplementedError('Model Arch {} Not Implemented'.format(config.name))
